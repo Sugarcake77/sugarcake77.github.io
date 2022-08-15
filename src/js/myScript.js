9f3e65ca9b0e5067ce534f0515bfc667225b5fee
SugarLin <106429154+Sugarcake77@users.noreply.github.com>
2022-08-15 17:39:48 +0500

    Add JS, JQuery

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -1,54 +1,25 @@
-let site, design, adapt;
-let calculator = {
-    price: [
-        [5000, 40000, 10000, 20000],
-        [2000, 5000, 10000],
-        [1000, 2000, 3000],
-    ],
-    days: [
-        [1, 10, 2, 4],
-        [1, 3, 5],
-        [1, 2, 3],
-    ],
-    run(siteType, designType, adaptType){
-        let sum = parseInt(calculator.price[0][siteType-1]) + parseInt(calculator.price[1][designType-1]) + parseInt(calculator.price[2][adaptType-1]);
-        let days = parseInt(calculator.days[0][siteType-1]) + parseInt(calculator.days[1][designType-1]) + parseInt(calculator.days[2][adaptType-1]);
-        alert("Стоимость: "+ sum +"\nСроки: "+ days);
-    }
-}
+$(document).ready(function () {
 
-function getAnswers(){
-    site = prompt("Выберите тип сайта: \n1 - Лэндинг \n2 - Интернет- м-н \n3 - Сайт-визитка \n4 - Корп.сайт");
-        if(site != 1 && site != 2 && site != 3 && site != 4 ){
-            alert("Некорректный ввод");
-            getAnswers();
-            return;
-        }
-    design = prompt("Адаптивность: \n1 - Шаблонный, \n2 -  Уникальный, \n3 - WOW-Дизайн");
-        if(design != 1 && design != 2 && design != 3 ){
-            alert("Некорректный ввод");
-            getAnswers();
-            return;  
-        }
-    adapt = prompt("Адаптивность: \n1 - Только - ПК \n2 - Мобильная версия \n3 - ПК + Мобильная");
-        if(adapt != 1 && adapt != 2 && adapt != 3 ){
-            alert("Некорректный ввод");
-            getAnswers();
-            return; 
+    function calculate() {
+        let sum = parseInt($("#select1 option:selected").val()) + parseInt($("#select2 option:selected").val()) + parseInt($("#select3 option:selected").val());
+        let days = parseInt($("#select1 option:selected").attr("days")) + parseInt($("#select2 option:selected").attr("days")) + parseInt($("#select3 option:selected").attr("days"));
+        $(".days .digit").text(days);
+        $(".stoimost .digit").text(sum);
     }
-    calculator.run(site, design, adapt);
-}
-getAnswers();
-    
- //   var siteType = prompt("Выберите тип сайта (стереть лишнее)", "Лэндинг/Интернет- м-н/Сайт-визитка/Корп.сайт");
-//        if (siteType === ''){alert("Вы не указали тип сайта");}
-//            else if (siteType === "Лэндинг"){alert(lend);} 
-//            else if (siteType === "Интернет- м-н"){alert(internetM);}
-//            else if (siteType === "Сайт-визитка"){alert(visit);}
-//            else if (siteType === "Корп.сайт"){alert(corp);}
-             
+    $("select").on("change", function () {
+        calculate();
+    });
+    calculate();
+});
 
-//var adapt = prompt("Требуется ли адаптивность вашему сайту?", "Да/Нет");
-//if (adapt === ''){alert("Вы не указали адаптивность сайта");}
-//    else if (adapt === "Да"){alert(yes);}
-//    else if (adapt === "Нет"){alert(no);}
+$('.count').each(function () {
+    $(this).prop('Counter', 0).animate({
+            Counter: $(this).data('value')
+        }, {
+        duration: 1000,
+        easing: 'swing',
+        step: function (now) {                      
+            $(this).text(this.Counter.toFixed(2));
+        }
+    });
+});
