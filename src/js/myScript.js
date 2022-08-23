4a70aa85119ad1adb945d191695c595dc58bb85f
SugarLin <106429154+Sugarcake77@users.noreply.github.com>
2022-08-23 14:24:49 +0500

    Работа с js кодом
    исправление ошибок

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -10,10 +10,10 @@ $(document).ready(function () {
         calculate();
     });
     calculate();
-    
+
     $(window).scroll(() => {
         let scrollDistance = $(window).scrollTop();
-        
+
         $('.section').each((i, el) => {
             if ($(el).offset().top - $('nav').outerHeight() <= scrollDistance) {
                 $('nav a').each((i, el) => {
@@ -25,9 +25,57 @@ $(document).ready(function () {
             }
         });
     });
+
+    $('a[href^="#"]').click(function () {
+        let anchor = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(anchor).offset().top
+        }, 600);
+    });
+
+        var windowHeight = $(window).height();
+
+        $(document).on('scroll', function () {
+            $('.section').each(function () {
+                var self = $(this),
+                    height = self.offset().top + self.height() / 2 - windowHeight / 2;
+                if ($(document).scrollTop() >= height) {
+                    self.addClass('green');
+                }
+            });
+        });
+    
+        let options = {
+            threshold: [0.5]
+        };
+        let observer = new IntersectionObserver(onEntry, options);
+        let elements = $('.element-animation');
+
+        elements.each((i, el) => {
+            observer.observe(el);
+        });
+
+
+        function onEntry(entry) {
+            entry.forEach(change => {
+                if (change.isIntersecting) {
+                    change.target.classList.add('element-show');
+                }
+            });
+        }
+    
+        $('.image-link').magnificPopup({type:'image'});
 });
 
 
-
-
-
+$('.count').each(function () {
+    $(this).prop('counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 5000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
