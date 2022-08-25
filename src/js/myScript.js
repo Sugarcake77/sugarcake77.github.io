62f09e23a3fa5459ceeac97f280d34b453d86bba
SugarLin <106429154+Sugarcake77@users.noreply.github.com>
2022-08-25 21:07:31 +0500

    Доделан JS
    Добавлены фото дозагрузки, исправлен CSS

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -36,60 +36,78 @@ $(document).ready(function () {
         }, 600);
     });
     
-        //анимация при скролле
-        let options = {
-            threshold: [0.5]
-        };
-        let observer = new IntersectionObserver(onEntry, options);
-        let elements = $('.element-animation');
+    //анимация при скролле
+    let options = {
+        threshold: [0.5]
+    };
+    let observer = new IntersectionObserver(onEntry, options);
+    let elements = $('.element-animation');
 
-        elements.each((i, el) => {
-            observer.observe(el);
-        });
-
-
-        function onEntry(entry) {
-            entry.forEach(change => {
-                if (change.isIntersecting) {
-                    change.target.classList.add('element-show');
-                }
-            });
-        }
-        //модальное окно в кейсах
-        $('.image-link').magnificPopup({type:'image'});
-    
-        //загрузка картинок при скролле
-        let optionsImg = {
-            threshold: [0.5]
-        };
-        let observerImg = new IntersectionObserver(onEntryImg, optionsImg);
-        let elementsImg = $('.lazy_image');
-
-        elementsImg.each((i, el) => {
-            observerImg.observe(el);
-        });
-
-
-        function onEntryImg(entry) {
-            entry.forEach(change => {
-                if (change.isIntersecting) {
-                    change.target.src = change.target.dataset.src;
-                }
-            });
-        }
-    
-    
-});
-
-//бегающие цифры
-$('.count').each(function () {
-    $(this).prop('counter',0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 5000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
+    elements.each((i, el) => {
+        observer.observe(el);
     });
+
+
+    function onEntry(entry) {
+        entry.forEach(change => {
+            if (change.isIntersecting) {
+                change.target.classList.add('element-show');
+            }
+        });
+    }
+    //модальное окно в кейсах
+    $('.image-link').magnificPopup({type:'image'});
+
+    //загрузка картинок при скролле
+    let optionsImg = {
+        threshold: [0.5]
+    };
+    let observerImg = new IntersectionObserver(onEntryImg, optionsImg);
+    let elementsImg = $('.lazy_image');
+
+    elementsImg.each((i, el) => {
+        observerImg.observe(el);
+    });
+
+
+    function onEntryImg(entry) {
+        entry.forEach(change => {
+            if (change.isIntersecting) {
+                change.target.src = change.target.getAttribute('data-src');
+            }
+        });
+    }
+    
+    let isCounterStart = false
+    let optionsCount = {
+        threshold: [0.5]
+    };
+    let observerCount = new IntersectionObserver(onEntryCount, optionsCount);
+    let elementsCount = $('.stat__container');
+
+    elementsCount.each((i, el) => {
+        observerCount.observe(el);
+    });
+
+
+    function onEntryCount(entry) {
+        entry.forEach(change => {
+            if (change.isIntersecting && !isCounterStart) {
+                isCounterStart = true
+                $('.count').each(function () {
+                    $(this).prop('counter',0).animate({
+                        Counter: $(this).text()
+                    }, {
+                        duration: 5000,
+                        easing: 'swing',
+                        step: function (now) {
+                            $(this).text(Math.ceil(now));
+                        }
+                    });
+                });
+            }
+        });
+    }
+    
+    
 });
